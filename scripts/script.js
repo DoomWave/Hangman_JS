f988c45d6dd840024ba3b40ab88863b6a163dd46
Najair Acuna <123853739+DoomWave@users.noreply.github.com>
2025-03-12 10:59:59 -0700

    Working in the JavaScript Part

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+const wordDisplay = document.querySelector(".word-display");
+const guessesText = document.querySelector(".guesses-text b");
+const keyboardDiv = document.querySelector(".keyboard");
+const gameModal = document.querySelector(".game-modal");
+const playAgainBtn = gameModal.querySelector("button");
+
---
+++ scripts/word-list.js
@@ -0,0 +1,22 @@
+const wordList = [
+    {
+        word: "guitar",
+        hint: "A musical instrument with strings"
+    },
+    {
+        word: "Dog",
+        hint: "a fluffy animal who lives with you",
+    },
+    {
+        word: "Book",
+        hint: "a block made of paper",
+    },
+    {
+        word: "Computer",
+        hint: "A machine to code",
+    },
+    {
+        word: "SCLA",
+        hint: "A summer program",
+    },
+]
